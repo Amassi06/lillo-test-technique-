822577111e319b0d819cb12f1f9800e2e1195211
Massi <aidelmassi@gmail.com>
2026-04-15 01:33:00 +0200

    fix: migration stuck

--- billing_project/urls.py
+++ billing_project/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularSwaggerView, SpectacularRetrieveAPIView
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("billing.urls")),
-    path('api/schema/', SpectacularRetrieveAPIView.as_view(), name='schema'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
